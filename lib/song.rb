f33cbb9955336d63581eb43597aefdb8f01a294d
Vanessa Toro <vanetoro84@gmail.com>
2018-04-04 17:47:27 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,10 @@
 require 'pry'
 
 class Song
+  extend Memorable
   attr_accessor :name
   attr_reader :artist
+  
 
   @@songs = []
 
@@ -18,16 +20,8 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
-
   def to_param
     name.downcase.gsub(' ', '-')
-  end
-
-  def self.count
-    self.all.count
   end
 
   def self.all
