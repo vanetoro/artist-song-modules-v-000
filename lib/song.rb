0520c94d484d80719d6b9e12777ade71c5fd1c2a
Vanessa Toro <vanetoro84@gmail.com>
2018-04-04 20:54:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,17 +14,9 @@ class Song
     @@songs << self
   end
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
-
   def artist=(artist)
     @artist = artist
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 
   def self.all
     @@songs
