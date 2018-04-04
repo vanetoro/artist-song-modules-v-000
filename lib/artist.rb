0762d57cf86d604797910a1ff86f873efa02ef04
Vanessa Toro <vanetoro84@gmail.com>
2018-04-04 21:03:46 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
 class Artist
-  extend Memorable :: ClassMethods
+  extend Memorable::ClassMethods
   extend Findable
-  include Memorable :: InstanceMethods
+  include Memorable::InstanceMethods
   include Paramable
   attr_accessor :name
   attr_reader :songs
